89f6e92fa1420c0374443591638de44c6f4bbe53
orianemgn <mgnoriane@gmail.com>
2022-09-22 17:28:54 +0200

    Started javaScript code since the beginning useg dom manipulation to get the array of people and size of the group withou handling event for now, group, groupSize, function numberOfGroup and function shuffledGroup working, devide the group still missing

--- scripts.js
+++ scripts.js
@@ -1,22 +1,72 @@
-let group = [
-  "Ahmed",
-  "Andrei",
-  "Georg",
-  "Hendra",
-  "Lars",
-  "Lera",
-  "Luis",
-  "Marianne",
-  "Mathieu",
-  "Mulugeta",
-  "Nevin",
-  "Oriane",
-  "Robert",
-  "Stefan D",
-  "Stefan I",
-  "Valeriia",
-  "Vladyslava"
-];
+//get the array of people from the form
+let group = document.getElementById('allNames'); 
+group = group.innerHTML.split(','); 
+console.log(group);
+
+//get the size of group of the form
+let groupSize = document.getElementById('groupSize');
+groupSize = groupSize.size
+console.log(groupSize);
+
+
+//find the number of group
+function numberOfGroup(group, groupSize){
+
+   //define the number of groups
+   let numberOfGroups;
+   //find out if the rest of people is an own group or needs to be distributed
+   if (group.length % groupSize <= groupSize / 2) {
+     //bsp 4er gruppe
+     numberOfGroups = group.length / groupSize;
+     // = 17/4 = 4,25
+     numberOfGroups = numberOfGroups - (numberOfGroups % 1);
+     // 4,25 - (0,25) =4
+   } else {
+     numberOfGroups = group.length / groupSize;
+     numberOfGroups = numberOfGroups - (numberOfGroups % 1) + 1;
+     // 19/4 = 4,75
+     // = 4,75 - 0,75 + 1 = 5
+   }
+  
+     console.log("Number of groups: " + numberOfGroups);
+     return numberOfGroups;
+
+}
+
+
+console.log(numberOfGroup(group,groupSize));
+
+
+ //shuffle the group names
+ let shuffledGroup = group.sort(function () {
+   return Math.random() - 0.5;
+ });
+ console.log("Shuffled array: ", shuffledGroup);
+
+
+
+
+
+// let group = [
+//   "Ahmed",
+//   "Andrei",
+//   "Georg",
+//   "Hendra",
+//   "Lars",
+//   "Lera",
+//   "Luis",
+//   "Marianne",
+//   "Mathieu",
+//   "Mulugeta",
+//   "Nevin",
+//   "Oriane",
+//   "Robert",
+//   "Stefan D",
+//   "Stefan I",
+//   "Valeriia",
+//   "Vladyslava"
+// ];
+
 // let groupSize = prompt("How many people should be in a group?");
 
 // //shuffle the group names
@@ -80,15 +130,15 @@ let group = [
 // // make it pretty with CSS
 
 //shuffle the group names
-let shuffledGroup = group.sort(function () {
-  return Math.random() - 0.5;
-});
+// let shuffledGroup = group.sort(function () {
+//   return Math.random() - 0.5;
+// });
 
-//create chunks
-const chunkSize = 5;
-for (let i = 0; i < shuffledGroup.length; i += chunkSize) {
-  const chunk = shuffledGroup.slice(i, i + chunkSize);
-  const displayResult = document.createElement("div");
-  displayResult.innerText = chunk;
-  document.querySelector("div").appendChild(displayResult);
-}
+// //create chunks
+// const chunkSize = 5;
+// for (let i = 0; i < shuffledGroup.length; i += chunkSize) {
+//   const chunk = shuffledGroup.slice(i, i + chunkSize);
+//   const displayResult = document.createElement("div");
+//   displayResult.innerText = chunk;
+//   document.querySelector("div").appendChild(displayResult);
+// }
